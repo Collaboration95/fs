6bc9d5ebcd85ac36d6f1e6b4a45c19519bfda665
Guru Prasath Gopal <guruprasath3200@gmail.com>
2023-09-05 15:10:43 +0800

    Finished Part1/1.2
    Finished Part1/1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,10 +9,18 @@ const Header = (props) => {
 const Content = (props) => {
   return  ( 
   <>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
   </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
   )
 }
 
@@ -23,6 +31,7 @@ const Total = (props) => {
     </>
   )
 }
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
