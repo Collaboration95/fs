3fd89366146bc601d3f66b270e538a5c70ac0744
Guruprasath <guruprasath3200@gmail.com>
2023-09-09 21:21:49 +0800

    Finished part2/2.11

--- part2/phbook/src/App.jsx
+++ part2/phbook/src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
-
+import React, { useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PhoneBook from './components/personform'
 import Persons from './components/persons'
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ])
+  const [persons, setPersons] = useState([])
+
+  const laodperson=()=>{
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response=>{
+      setPersons(response.data)
+      console.log("Data fetched from db.json")
+    })
+  }
+  useEffect(laodperson,[])
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [newFilter, setFilter] = useState('')
